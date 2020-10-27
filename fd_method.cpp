2462779a9c0729cb387f109105847911d176bb56
Daniel Estrada <42388093+DanielEstrada971102@users.noreply.github.com>
2020-10-26 23:46:56 -0500

    Update fd_method.cpp
    Se comentaron los pasos del algoritmo según la descripción del libro

--- fd_method.cpp
+++ fd_method.cpp
@@ -50,12 +50,14 @@ void FiniteDiff::solve(double *x, double *w){
 void FiniteDiff::calcule_EqSys(double *x){
 	int i;
 	x[0] = a;
+	// ------ step 1 -----------------
 	x[1] = a + h;
 
 	A.push_back( 2  + h*h * q(x[1]));
 	B.push_back( -1  + .5 * h * p(x[1]));	
 	D.push_back( - h*h * r(x[1]) + ((1 + .5 * h) * p(x[1])) * alpha );
-
+	
+	// ------ step 2 -----------------
 	for (i = 2; i <= n-1 ; i++){
 		x[i] = a + i * h;
 		A.push_back( 2  + h*h * q(x[i]));
@@ -63,7 +65,8 @@ void FiniteDiff::calcule_EqSys(double *x){
 		C.push_back( -1  - .5 * h * p(x[i]));	
 		D.push_back( - h*h * r(x[i]) );
 	}
-
+	
+	// ------ step 3 -----------------
 	x[n] = b - h;
 	A.push_back( 2  + h*h * q(x[n]));
 	C.push_back( -1  + .5 * h * p(x[n]));	
@@ -77,24 +80,27 @@ void FiniteDiff::croutFactorization(double *w){
 	double L[n+1];
 	double U[n+1];
 	double Z[n+1];
-
+	// ------ step 4 -----------------
 	L[1] = A[1];
 	U[1] = B[1] / A[1];	
 	Z[1] = D[1] / L[1];
-
+	
+	// ------ step 5 -----------------
 	for (i = 2; i <= n-1 ; i++){
 		L[i] =  A[i] - C[i] * U[i-1];
 		U[i] =  B[i] / L[i];
 		Z[i] = ( D[i] - C[i] * Z[i-1]) / L[i] ;	
 	}
-	
+	// ------ step 6 -----------------
 	L[n] = A[n] - C[n] * U[n-1];
 	Z[n] = ( D[n] - C[n] * Z[n-1]) / L[n];
 	
+	// ------ step 7 -----------------
 	w[0] = alpha;
 	w[n] = Z[n];
 	w[n+1] = beta;
 	
+	// ------ step 8 -----------------
 	for (i = n-1; i >= 1 ; i--){
 		w[i] = Z[i] - U[i] * w[i+1];	
 	}
